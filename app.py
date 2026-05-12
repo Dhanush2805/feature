f331320651ad7d7ffa89dd10b9ac5a096d2fb1e0
Dhanush2805 <xyz@usefulbi.com>
2026-05-13 01:00:11 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
 
 aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY"
 
-private_key = """
------BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAwFakeKeyTestingOnly123456789
-ZXhhbXBsZWtleQ==
------END RSA PRIVATE KEY-----
-"""
 
 print("talisman")
 
