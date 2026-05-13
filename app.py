08cc4b823ca0ae3f2c7a69396b8ed323e90722e8
Dhanush2805 <xyz@usefulbi.com>
2026-05-13 21:46:24 +0530

    updated app.py

--- app.py
+++ app.py
@@ -5,12 +5,8 @@ message = "This is only sample documentation"
 print("talisman")
 
 ########################
-class FakeCursor:
-    def execute(self, query):
-        print(query)
 
-cursor = FakeCursor()
 
 user_input = input()
-
-cursor.execute(f"SELECT * FROM users WHERE name = '{user_input}'")
+query = "SELECT * FROM users WHERE name = '" + user_input + "'"
+print(query)
