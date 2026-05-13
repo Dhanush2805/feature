47de770aba889e82d087999c7dceed1b33e5a353
Dhanush2805 <xyz@usefulbi.com>
2026-05-13 21:12:48 +0530

    updated app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ message = "This is only sample documentation"
 print("talisman")
 
 ########################
+user_input = input()
 
 queries = "SELECT * FROM users WHERE id=" + user_input
 cursor.execute(queries)
